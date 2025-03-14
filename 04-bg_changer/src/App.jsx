1f7b37d4c282ff3da4e2778ac29d6132882e20ef
asifmonda07 <asikbal7ai@gmail.com>
2025-03-14 23:03:03 +0530

    added

---
+++ 04-bg_changer/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [color,setColor]=useState("olive")
+
+  return (
+    <>
+      <div className='w-full h-screen duraction-200' style={{backgroundColor: color}}>
+        <div className='fixed flex flex-wrap justify-center botom-12 inset-x-0 px-2'>
+            <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+              <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
+              <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"green"}}onClick={()=>setColor("green")}>Green</button>
+              <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>blue</button>
+              <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"black"}} onClick={()=>setColor("black")}>black</button>
+            </div>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
