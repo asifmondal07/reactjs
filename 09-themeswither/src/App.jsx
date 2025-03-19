05b63a135f62e0c1e23965bc39c6f951b8ee76a0
asifmonda07 <asikbal7ai@gmail.com>
2025-03-19 22:31:36 +0530

    added themeswither

---
+++ 09-themeswither/src/App.jsx
@@ -0,0 +1,41 @@
+import React,{useState,useEffect} from "react"
+import './App.css'
+import { ThemeProvider } from "./context/Theme"
+import ThemeBtn from "./components/ThemeBtn"
+import Card from "./components/Card"
+
+function App() {
+  let [themeMode,setThemeMode]=useState("light")
+  let lightTheme=()=>{
+    setThemeMode("light")
+  }
+  let darkTheme=()=>{
+    setThemeMode("dark")
+  }
+
+  useEffect(() => {
+   document.querySelector('html').classList.remove("light","dark")
+   document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
+
+  return (
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+      
+              <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn />
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card />
+                    </div>
+                </div>
+              </div>
+
+    </ThemeProvider>
+  )
+}
+
+export default App
